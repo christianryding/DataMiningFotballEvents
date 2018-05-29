918d143082224d01616f495f334ce5e7436e1de4
christianryding <christian.ryding@gmail.com>
2018-05-29 13:03:15 +0200

    finished first three betting models

--- src/BettingModels.cpp
+++ src/BettingModels.cpp
@@ -25,10 +25,6 @@ BettingModels::~BettingModels(){
 }
 
 
-
-
-
-
 // bets same amount every time on draw every single game
 void BettingModels::betDrawEveryTime(double bettingSum, std::vector<std::vector<Event>> &allFotballEventsVec){
 
@@ -37,13 +33,7 @@ void BettingModels::betDrawEveryTime(double bettingSum, std::vector<std::vector<
   int drawCounter = 0;
 
   std::ofstream dataFile;
-  dataFile.open("draw.dat" , std::ios::trunc);
-  dataFile.close();
-  dataFile.open ("draw.dat", std::ios::app);
-
- 
-
-  double average = 0;
+  dataFile.open("draw.dat" , std::ios::trunc | std::ios::app);
   
   std::cout << std::endl << "Bet on draw every time" << std::endl;
   std::cout << "Starting sum: " << sum << std::endl;
@@ -52,31 +42,25 @@ void BettingModels::betDrawEveryTime(double bettingSum, std::vector<std::vector<
     for( int j = 0; j < allFotballEventsVec.at(i).size(); j++){
 
       // if draw, add winnings to sum
-      if(allFotballEventsVec.at(i).at(j).getFinalScoreHome() == allFotballEventsVec.at(i).at(j).getFinalScoreAway()){  
-	//totalSum = totalSum + allFotballEventsVec.at(i).at(j).getDrawOdds()*bettingSum;
+      if(allFotballEventsVec.at(i).at(j).getFinalScoreHome() == allFotballEventsVec.at(i).at(j).getFinalScoreAway()){
 	totalSum = totalSum + ( bettingSum * (allFotballEventsVec.at(i).at(j).getDrawOdds()-1) );
-	
 	drawCounter++;
 
 	// debug
-	dataFile << "Total: " << totalSum << std::endl
-		 << allFotballEventsVec.at(i).at(j).getEventID() << ";"
+	dataFile  << allFotballEventsVec.at(i).at(j).getEventID() << ";"
 		 << allFotballEventsVec.at(i).at(j).getMatchDate() << ";"
 		 << allFotballEventsVec.at(i).at(j).getDrawOdds() << ";"
 		 << std::endl << "Wins: " << allFotballEventsVec.at(i).at(j).getDrawOdds()*bettingSum << ";"
 		 << "Odds: " << allFotballEventsVec.at(i).at(j).getDrawOdds() << " * " << bettingSum << "="
 		 << allFotballEventsVec.at(i).at(j).getDrawOdds()*bettingSum << ";" << std:: endl
 		 << "Total: " << totalSum << std::endl << std::endl;
-
-	average += allFotballEventsVec.at(i).at(j).getDrawOdds();
       }
       // if not draw, withdraw bettingsum from banksum
       else{
 	totalSum = totalSum - bettingSum;
 
 	// debug
-	dataFile << "Total: " << totalSum << std::endl
-		 << allFotballEventsVec.at(i).at(j).getEventID() << ";"
+	dataFile  << allFotballEventsVec.at(i).at(j).getEventID() << ";"
 		 << allFotballEventsVec.at(i).at(j).getMatchDate() << " "
 		 << allFotballEventsVec.at(i).at(j).getDrawOdds() << " "
 		 << std::endl << "Lose: " << bettingSum << ";"
@@ -87,26 +71,10 @@ void BettingModels::betDrawEveryTime(double bettingSum, std::vector<std::vector<
     }
   }
 
-
   std::cout << "Draw matches: " << drawCounter << "/" << betCounter << std::endl;
-  std::cout << "Total bets placed: " << betCounter << std::endl;
   std::cout << "End sum: " << totalSum << std::endl;
-  std::cout << "Average Odds: " << average/drawCounter << std:: endl;//test
-  std::cout << "Calc winnings: " << 10*drawCounter*average;
-
   dataFile.close();
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 // bets same amount every time on home team every single game
@@ -117,9 +85,7 @@ void BettingModels::betHomeTeamEveryTime(double bettingSum, std::vector<std::vec
   int homeTeamCounter = 0;
 
   std::ofstream dataFile;
-  dataFile.open("home.dat" , std::ios::trunc);
-  dataFile.close();
-  dataFile.open ("home.dat", std::ios::app);
+  dataFile.open("home.dat" , std::ios::trunc | std::ios::app);
 
   std::cout << std::endl << "Bet on home team every time" << std::endl;
   std::cout << "Starting sum: " << sum << std::endl;
@@ -129,14 +95,11 @@ void BettingModels::betHomeTeamEveryTime(double bettingSum, std::vector<std::vec
 
       // if hometeam won, add winnings to sum
       if(allFotballEventsVec.at(i).at(j).getFinalScoreHome() > allFotballEventsVec.at(i).at(j).getFinalScoreAway()){  
-	//totalSum = totalSum + allFotballEventsVec.at(i).at(j).getHomeOdds()*bettingSum;
 	totalSum = totalSum + ( bettingSum * (allFotballEventsVec.at(i).at(j).getHomeOdds()-1) );
-	
 	homeTeamCounter++;
 
 	// debug
-	dataFile << "Total: " << totalSum << std::endl
-		 << allFotballEventsVec.at(i).at(j).getEventID() << ";"
+	dataFile << allFotballEventsVec.at(i).at(j).getEventID() << ";"
 		 << allFotballEventsVec.at(i).at(j).getMatchDate() << ";"
 		 << allFotballEventsVec.at(i).at(j).getHomeOdds() << ";"
 		 << std::endl << "Wins: " << allFotballEventsVec.at(i).at(j).getHomeOdds()*bettingSum << ";"
@@ -151,8 +114,7 @@ void BettingModels::betHomeTeamEveryTime(double bettingSum, std::vector<std::vec
 
 
 	// debug
-	dataFile << "Total: " << totalSum << std::endl
-		 << allFotballEventsVec.at(i).at(j).getEventID() << ";"
+	dataFile << allFotballEventsVec.at(i).at(j).getEventID() << ";"
 		 << allFotballEventsVec.at(i).at(j).getMatchDate() << " "
 		 << allFotballEventsVec.at(i).at(j).getHomeOdds() << " "
 		 << std::endl << "Lose: " << bettingSum << ";"
@@ -164,11 +126,8 @@ void BettingModels::betHomeTeamEveryTime(double bettingSum, std::vector<std::vec
     }
   }
 
-
   std::cout << "Hometeam Victories: " << homeTeamCounter << "/" << betCounter << std::endl;
-  std::cout << "Total bets placed: " << betCounter << std::endl;
   std::cout << "End sum: " << totalSum << std::endl;
-
   dataFile.close();
 }
 
@@ -181,9 +140,7 @@ void BettingModels::betAwayTeamEveryTime(double bettingSum, std::vector<std::vec
   int awayTeamCounter = 0;
 
   std::ofstream dataFile;
-  dataFile.open("away.dat" , std::ios::trunc);
-  dataFile.close();
-  dataFile.open ("away.dat", std::ios::app);
+  dataFile.open("away.dat" , std::ios::trunc | std::ios::app);
 
   std::cout << std::endl << "Bet on away team every time" << std::endl;
   std::cout << "Starting sum: " << sum << std::endl;
@@ -193,14 +150,11 @@ void BettingModels::betAwayTeamEveryTime(double bettingSum, std::vector<std::vec
 
       // if awayteam won, add winnings to sum
       if(allFotballEventsVec.at(i).at(j).getFinalScoreHome() < allFotballEventsVec.at(i).at(j).getFinalScoreAway()){  
-	//totalSum = totalSum + (allFotballEventsVec.at(i).at(j).getAwayOdds() * bettingSum);
 	totalSum = totalSum + ( bettingSum * (allFotballEventsVec.at(i).at(j).getAwayOdds()-1) );
-	
 	awayTeamCounter++;
 
 	// debug
-	dataFile << "Total: " << totalSum << std::endl
-		 << allFotballEventsVec.at(i).at(j).getEventID() << ";"
+	dataFile << allFotballEventsVec.at(i).at(j).getEventID() << ";"
 		 << allFotballEventsVec.at(i).at(j).getMatchDate() << ";"
 		 << allFotballEventsVec.at(i).at(j).getAwayOdds() << ";"
 		 << std::endl << "Wins: " << allFotballEventsVec.at(i).at(j).getAwayOdds()*bettingSum << ";"
@@ -214,8 +168,7 @@ void BettingModels::betAwayTeamEveryTime(double bettingSum, std::vector<std::vec
 	totalSum = totalSum - bettingSum;
 
 	// debug
-	dataFile << "Total: " << totalSum << std::endl
-		 << allFotballEventsVec.at(i).at(j).getEventID() << ";"
+	dataFile << allFotballEventsVec.at(i).at(j).getEventID() << ";"
 		 << allFotballEventsVec.at(i).at(j).getMatchDate() << " "
 		 << allFotballEventsVec.at(i).at(j).getAwayOdds() << " "
 		 << std::endl << "Lose: " << bettingSum << ";"
@@ -228,8 +181,6 @@ void BettingModels::betAwayTeamEveryTime(double bettingSum, std::vector<std::vec
   }
 
   std::cout << "Hometeam Victories: " << awayTeamCounter << "/" << betCounter << std::endl;
-  std::cout << "Total bets placed: " << betCounter << std::endl;
   std::cout << "End sum: " << totalSum << std::endl;
-
   dataFile.close();
 }
